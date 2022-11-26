a483175713dd1fa9d3db359e4946254f54282c49
long2k <truongquanglong777@gmail.com>
2022-11-26 09:32:36 +0700

    list product

---
+++ src/controllers/product/list.js
@@ -0,0 +1,12 @@
+const Product = require('../../models/product.model')
+module.exports = async (req, res) => {
+    try {
+        
+        const product = await Product.find()
+        if (product) {
+            return res.ok(product)
+        }
+    } catch (error) {
+        return res.serverError("error:", error)
+    }
+}
--- src/routers/route/product.routes.js
+++ src/routers/route/product.routes.js
@@ -5,6 +5,7 @@ const Router = express.Router();
 Router.post('/create', require('../../controllers/product/create'))
 Router.post('/edit', require('../../controllers/product/edit'))
 Router.post('/delete', require('../../controllers/product/delete'))
+Router.get('/', require('../../controllers/product/list'))
 
 
 module.exports = Router;
