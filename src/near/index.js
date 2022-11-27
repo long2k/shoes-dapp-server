0df12229912458794e9c060882c940d5b67b823e
long2k <truongquanglong777@gmail.com>
2022-11-27 21:26:42 +0700

    update near and link mongo

--- src/near/index.js
+++ src/near/index.js
@@ -5,8 +5,8 @@ const { KeyPair, keyStores, connect } = require("near-api-js");
 const fs = require("fs");
 const homedir = require("os").homedir();
 
-export const ACCOUNT_ID = process.env.ACCOUNT_ID; // NEAR account tied to the keyPair
-export const NETWORK_ID = "testnet";
+ const ACCOUNT_ID = process.env.ACCOUNT_ID; // NEAR account tied to the keyPair
+ const NETWORK_ID = "testnet";
 // path to your custom keyPair location (ex. function access key for example account)
 const KEY_PATH = `/.near-credentials/testnet/${ACCOUNT_ID}.json`;
 
@@ -26,8 +26,16 @@ const connectionConfig = {
     helperUrl: "https://helper.testnet.near.org",
     explorerUrl: "https://explorer.testnet.near.org",
 };
-export const account = (async () => {
+ const account = (async () => {
     const nearConnection = await connect(connectionConfig);
     return await nearConnection.account(ACCOUNT_ID);
 })();
-export const keyStore = myKeyStore;
+ const keyStore = myKeyStore;
+
+
+module.exports = {
+    account,
+    keyStore,
+    ACCOUNT_ID,
+    NETWORK_ID
+}
